7e4cea628b395509f1ee171b8b47c9811e406d9d
moulayprogrammer <moulay0311@gmail.com>
2022-10-27 17:46:36 +0200

    Consumption Status

--- src/Controllers/OutputControllers/ConsumptionStatusController.java
+++ src/Controllers/OutputControllers/ConsumptionStatusController.java
@@ -17,23 +17,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.controlsfx.control.ListSelectionView;
 
+
 import java.net.URL;
-import java.sql.Connection;
+import java.sql.*;
 import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ConsumptionStatusController implements Initializable {
 
+    @FXML
+    HBox hBox;
     @FXML
     DatePicker dpDate;
     @FXML
@@ -76,7 +75,6 @@ public class ConsumptionStatusController implements Initializable {
 
         // init date
         dpDate.setValue(LocalDate.now());
-
     }
 
     private void refreshListSelectionCategory() {
@@ -151,7 +149,8 @@ public class ConsumptionStatusController implements Initializable {
 
             if (date != null && lsCategorySelected.size() != 0){
 
-                HashMap<Integer,List<List<StringProperty>>> cons = new HashMap<>();
+                HashMap<String,List<List<StringProperty>>> cons = new HashMap<>();
+                HashMap<String,Double> consGas = new HashMap<>();
                 int indexDep = cbDep.getSelectionModel().getSelectedIndex();
                 int indexServ = cbServ.getSelectionModel().getSelectedIndex();
 
@@ -163,11 +162,10 @@ public class ConsumptionStatusController implements Initializable {
                             PreparedStatement preparedStmt = conn.prepareStatement(query);
                             preparedStmt.setDate(1, Date.valueOf(date));
                             preparedStmt.setInt(2,services.get(indexServ).getId());
-                            System.out.println("serv = " + services.get(indexServ).getId());
                             ResultSet resultSet = preparedStmt.executeQuery();
 
                             while (resultSet.next()){
-                                cons.put(resultSet.getInt("ID"),new ArrayList<>());
+                                cons.put(String.valueOf(resultSet.getInt("ID")),new ArrayList<>());
                             }
                         }catch (Exception e){
                             e.printStackTrace();
@@ -176,14 +174,14 @@ public class ConsumptionStatusController implements Initializable {
                         // select dep
                         try {
                             String query = "SELECT OUTPUT.ID FROM OUTPUT,EMPLOYEE,SERVICE WHERE OUTPUT.DATE = ? " +
-                                    "AND SERVICE.ID_DEP = ? AND EMPLOYEE.ID_SERVICE = SERVICE.ID AND OUTPUT.ID_EMP = EMPLOYEE.ID ;";
+                                    "AND SERVICE.ID_DEP     = ? AND EMPLOYEE.ID_SERVICE = SERVICE.ID AND OUTPUT.ID_EMP = EMPLOYEE.ID ;";
                             PreparedStatement preparedStmt = conn.prepareStatement(query);
                             preparedStmt.setDate(1, Date.valueOf(date));
                             preparedStmt.setInt(2,departments.get(indexDep).getId());
                             ResultSet resultSet = preparedStmt.executeQuery();
 
                             while (resultSet.next()){
-                                cons.put(resultSet.getInt("ID"),new ArrayList<>());
+                                cons.put(String.valueOf(resultSet.getInt("ID")),new ArrayList<>());
                             }
                         }catch (Exception e){
                             e.printStackTrace();
@@ -198,83 +196,145 @@ public class ConsumptionStatusController implements Initializable {
                         ResultSet resultSet = preparedStmt.executeQuery();
 
                         while (resultSet.next()){
-                            cons.put(resultSet.getInt("ID"),new ArrayList<>());
+                            cons.put(String.valueOf(resultSet.getInt("ID")),new ArrayList<>());
                         }
                     }catch (Exception e){
                         e.printStackTrace();
                     }
                 }
-
+                // select other cat not gasoline
                 if (cons.size() != 0){
+                    List<String> bsVide = new ArrayList<>();
+                    List<String> bsNotVide = new ArrayList<>();
                     cons.forEach((s, stringProperties) -> {
                         try {
                             List<List<StringProperty>> data = new ArrayList<>();
+                            double totBs = 0.0;
+                            String nbBs = "";
+
                             for (String cat : lsCategorySelected) {
-                                int index = categoryList.indexOf(cat);
-                                int idCat = categories.get(index).getId();
+                                if (!cat.equals("Carburants")) {
+                                    int index = categoryList.indexOf(cat);
+                                    int idCat = categories.get(index).getId();
 
-                                String query = "SELECT OUTPUT.NUMBER, SUM(STORE_CARD.PRICE * COMPONENT_OUTPUT.QTE_SERV) AS TOT FROM OUTPUT,STORE_CARD, COMPONENT_OUTPUT, ARTICLE \n" +
-                                        "WHERE OUTPUT.ID = ? AND COMPONENT_OUTPUT.ID_OUTPUT = OUTPUT.ID AND ARTICLE.ID_CAT = ? AND COMPONENT_OUTPUT.ID_ART = ARTICLE.ID \n" +
-                                        "AND COMPONENT_OUTPUT.ID_STORE = STORE_CARD.ID";
+                                    String query = "SELECT OUTPUT.NUMBER, SUM(STORE_CARD.PRICE * COMPONENT_OUTPUT.QTE_SERV) AS TOT FROM OUTPUT,STORE_CARD, COMPONENT_OUTPUT, ARTICLE \n" +
+                                            "WHERE OUTPUT.ID = ? AND COMPONENT_OUTPUT.ID_OUTPUT = OUTPUT.ID AND ARTICLE.ID_CAT = ? AND COMPONENT_OUTPUT.ID_ART = ARTICLE.ID \n" +
+                                            "AND COMPONENT_OUTPUT.ID_STORE = STORE_CARD.ID";
 
-                                PreparedStatement preparedStmt = conn.prepareStatement(query);
-                                preparedStmt.setInt(1, s);
-                                preparedStmt.setInt(2, idCat);
+                                    PreparedStatement preparedStmt = conn.prepareStatement(query);
+                                    preparedStmt.setInt(1, Integer.parseInt(s));
+                                    preparedStmt.setInt(2, idCat);
 
-                                ResultSet resultSet = preparedStmt.executeQuery();
-                                while (resultSet.next()) {
-                                    List<StringProperty> d = new ArrayList<>();
+                                    ResultSet resultSet = preparedStmt.executeQuery();
 
-                                    d.add(new SimpleStringProperty(resultSet.getString("NUMBER")));
-                                    d.add(new SimpleStringProperty(categories.get(index).getName()));
-                                    d.add(new SimpleStringProperty(String.valueOf(resultSet.getDouble("TOT"))));
+                                    while (resultSet.next()) {
+                                        List<StringProperty> d = new ArrayList<>();
 
-                                    data.add(d);
+                                        double tot = resultSet.getDouble("TOT");
+                                        totBs += tot;
+                                        nbBs = resultSet.getString("NUMBER");
+                                        d.add(new SimpleStringProperty(resultSet.getString("NUMBER")));
+                                        d.add(new SimpleStringProperty(categories.get(index).getName()));
+                                        d.add(new SimpleStringProperty(String.valueOf(tot)));
+
+                                        data.add(d);
+                                    }
                                 }
                             }
-                            cons.put(s,data);
+                            if (totBs != 0) {
+                                cons.put(s,data);
+                                bsNotVide.add(s);
+                            }else bsVide.add(s);
                         }catch (Exception e){
                             e.printStackTrace();
                         }
                     });
+                    // clean hashMap
+                    bsVide.forEach(cons::remove);
+                    bsNotVide.forEach(s -> {
+                        List<List<StringProperty>> data = new ArrayList<>(cons.get(s));
+                        cons.remove(s);
+                        String nb = data.get(0).get(0).get();
+
+                        for (int i = 0; i < data.size(); i++) {
+                            data.get(i).remove(0);
+                        }
+                        cons.put(nb,data);
+                    });
                 }
 
-                cons.forEach((integer, lists) -> {
-                    System.out.println("output = " + lists.get(0).get(0).getValue());
-                    for (List<StringProperty> list : lists){
-                        System.out.print("Cat = " + list.get(1).getValue());
-                        System.out.print("    Tot = " + list.get(2).getValue());
-                        System.out.print("    ||");
+                // select gasoline
+                if (lsCategorySelected.contains("Carburants")){
+                    if (indexDep != -1){
+                        if (indexServ != -1){
+                            // select dep && serv
+                            try {
+                                String query = "SELECT OUTPUT.ID FROM OUTPUT,EMPLOYEE,SERVICE WHERE OUTPUT.DATE = ? AND EMPLOYEE.ID_SERVICE = ? AND OUTPUT.ID_EMP = EMPLOYEE.ID ;";
+                                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                                preparedStmt.setDate(1, Date.valueOf(date));
+                                preparedStmt.setInt(2,services.get(indexServ).getId());
+                                ResultSet resultSet = preparedStmt.executeQuery();
+
+                                while (resultSet.next()){
+                                    cons.put(String.valueOf(resultSet.getInt("ID")),new ArrayList<>());
+                                }
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }else {
+                            // select dep
+                            try {
+                                String query = "SELECT OUTPUT.ID FROM OUTPUT,EMPLOYEE,SERVICE WHERE OUTPUT.DATE = ? " +
+                                        "AND SERVICE.ID_DEP     = ? AND EMPLOYEE.ID_SERVICE = SERVICE.ID AND OUTPUT.ID_EMP = EMPLOYEE.ID ;";
+                                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                                preparedStmt.setDate(1, Date.valueOf(date));
+                                preparedStmt.setInt(2,departments.get(indexDep).getId());
+                                ResultSet resultSet = preparedStmt.executeQuery();
+
+                                while (resultSet.next()){
+                                    cons.put(String.valueOf(resultSet.getInt("ID")),new ArrayList<>());
+                                }
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }else{
+                        // not select
+                        try {
+                            String query = "SELECT NUMBER, PRICE FROM RECHARGE_GASOLINE_CARD WHERE DATE = ?;";
+                            PreparedStatement preparedStmt = conn.prepareStatement(query);
+                            preparedStmt.setDate(1, Date.valueOf(date));
+                            ResultSet resultSet = preparedStmt.executeQuery();
+
+                            while (resultSet.next()){
+                                consGas.put(String.valueOf(resultSet.getString("NUMBER")),resultSet.getDouble("PRICE"));
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
                     }
-                    System.out.println();
+                }
+
+                TreeSet<String> treeSet = new TreeSet<>();
+                if (cons.size() != 0) treeSet.addAll(cons.keySet());
+                if (consGas.size() != 0) treeSet.addAll(consGas.keySet());
+
+                treeSet.forEach(s -> {
+                    if (cons.containsKey(s)){
+                        List<List<StringProperty>> lists = cons.get(s);
+                        System.out.print("output = " + s + "     ");
+                        for (List<StringProperty> list : lists){
+                            System.out.print("Cat = " + list.get(0).getValue());
+                            System.out.print("    Tot = " + list.get(1).getValue());
+                            System.out.print("    ||");
+                        }
+                        System.out.println();
+                    }else {
+                        System.out.print("gas = " + s);
+                        System.out.print("     PRICE  = " + consGas.get(s));
+                        System.out.println();
+                    }
                 });
-
-
-
-                /*for (String cat : lsCategorySelected){
-                    int index = categoryList.indexOf(cat);
-                    int idCat = categories.get(index).getId();
-
-                    String query = "SELECT OUTPUT.NUMBER, \n" +
-                            "(SELECT SUM(STORE_CARD.PRICE * COMPONENT_OUTPUT.QTE_SERV) FROM STORE_CARD, COMPONENT_OUTPUT, ARTICLE \n" +
-                            "WHERE ARTICLE.ID_CAT = ? AND COMPONENT_OUTPUT.ID_ART = ARTICLE.ID AND COMPONENT_OUTPUT.ID_OUTPUT = OUTPUT.ID \n" +
-                            "AND COMPONENT_OUTPUT.ID_STORE = STORE_CARD.ID) AS TOT_OUTPUT FROM OUTPUT WHERE OUTPUT.DATE = ?;";
-
-                    PreparedStatement preparedStmt = conn.prepareStatement(query);
-                    preparedStmt.setInt(1,idCat);
-                    preparedStmt.setDate(2, Date.valueOf(date));
-                    ResultSet resultSet = preparedStmt.executeQuery();
-                    while (resultSet.next()){
-                        List<StringProperty> d = new ArrayList<>();
-
-                        d.add( new SimpleStringProperty(resultSet.getString("NUMBER")));
-                        d.add( new SimpleStringProperty(categories.get(index).getName()));
-                        d.add( new SimpleStringProperty(String.valueOf(resultSet.getDouble("TOT_OUTPUT"))));
-
-                        data.add(d);
-                    }
-                }*/
-
 
             }
         }catch (Exception e){
